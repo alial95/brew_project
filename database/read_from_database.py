a29f500efaa203454e5a036c553e5a8cd4a8d3e6
Alistair <alistair_j_cooper@hotmail.com>
2020-09-09 15:36:19 +0100

    cleaned up read from database functions

--- database/read_from_database.py
+++ database/read_from_database.py
@@ -1,26 +1,5 @@
 import pymysql
-people_list = []
 
-class Person:
-    def __init__(self, name, age):
-        """Get the human's name and age."""
-        self.name = name
-        self.age = age
-    def __repr__(self):
-        return f'{self.name}, {self.age}'
-
-
-class Drink:
-    def __init__(self, drink_name, container, volume):
-        self.drink_name = drink_name
-        self.container = container
-        self.volume = int(volume)
-
-    def have_drink(self, volume_poured):
-        self.volume -= volume_poured
-
-    def __repr__(self):
-        return (f"{self.drink_name}, {self.container}, {self.volume}")
 
 def read_database_people(list_name):
     connection = pymysql.connect(
@@ -30,18 +9,14 @@ def read_database_people(list_name):
 		"ali_test"
 	)
     cursor = connection.cursor()
-    cursor.execute("SELECT person_id, first_name, age FROM person")
+    cursor.execute("SELECT person_id, person_name, age FROM person")
     rows = cursor.fetchall()
     for row in rows:
         person = Person(row[1], row[2])
         list_name.append(person)
-        # print("ID - " + str(row[0]) + ", Name - " + row[1] + ', Age -' + str(row[2]))
     cursor.close()
     connection.close()
     return list_name
-
-read_database_people(people_list)
-print(people_list)
 
 def read_database_drinks(list_name):
     connection = pymysql.connect(
@@ -51,7 +26,7 @@ def read_database_drinks(list_name):
 		"ali_test"
 	)
     cursor = connection.cursor()
-    cursor.execute("SELECT drink_id, drink_name, drink_container, volume FROM drinks")
+    cursor.execute("SELECT drink_id, drink_name, container, volume FROM drinks")
     rows = cursor.fetchall()
     for row in rows:
         drink = Drink(row[1], row[2], row[3])

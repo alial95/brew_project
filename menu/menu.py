e9d0f99f4048c1c41d17669b1f33d4ac8bb1b423
Alistair <alistair_j_cooper@hotmail.com>
2020-09-16 10:13:42 +0100

    updated the menu

--- menu/menu.py
+++ menu/menu.py
@@ -9,12 +9,10 @@ Please, select an option below by entering a number:
     [2] Make new person from user input
     [3] Print list of people
     [4] Print list of drinks
-    [5] Save list to file
-    [6] Make round 
-    [7] Select preference
-    [8] Print preference list
-    [9] Save preference list to file
-    [10] Exit
+    [5] Make round 
+    [6] Select preference
+    [7] Print preference list
+    [8] Exit and save to DB
     """
     print(menu_text)
     while True:
